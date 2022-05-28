45d65922ef37c01fadb5d32105ad1cc5f787d365
Jennika Wilson <j.r.a.112100@gmail.com>
2022-05-28 14:31:40 -0400

    Added comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,32 +1,99 @@
-const React = require('react')
-const Def = require('../default')
+const React = require("react");
+const Def = require("../default");
 
-function show (data) {
-    return (
-        <Def>
-          <main>
-            <section className='show-sec'>
-            <img className='show-img' src={data.place.pic} alt={data.place.name} />
+function show(data) {
+  let comments = <h3 className="inactive">No comments yet!</h3>;
+  if (data.place.comments.length) {
+    comments = data.place.comments.map((c) => {
+      return (
+        <div className="border">
+          <h2 className="rant">{c.rant ? "Rant! ðŸ˜¡" : "Rave! ðŸ˜»"}</h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <stong>- {c.author}</stong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      );
+    });
+  }
+  return (
+    <Def>
+      <main>
+        <div className="row">
+          <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
             <h1>{data.place.name}</h1>
-            <h3>Located in {data.place.city}, {data.place.state}</h3>
-            <div className='show-ratedisc'>
-              <div className='show-rate'>Rating</div>
-              <div className='show-disc'>Discription</div>
-              <div>{data.place.showEstablished()}</div>
-              <div>{data.place.cuisines}</div>
+            <h2>Rating</h2>
+            No ratings yet
+            <br />
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
+            <br />
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+              Edit
+            </a>
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form>
+          </div>
+        </div>
+        <hr />
+        <h2 className="show-com">Comments</h2>
+        <div className="row">{comments}</div>
+        <hr />
+        <h2>Got Your Own Rant or Rave?</h2>
+        <form action={`/places/${data.place.id}/comment`} method="POST">
+          <div className="row">
+            <div className="form-group col-sm-12">
+              <label htmlFor="content">Content</label>
+              <textarea
+                id="content"
+                name="content"
+                className="form-control"
+              ></textarea>
             </div>
-            </section>
-            <div className='show-com'>Comments</div>
-            <div className='show-btn'>
-              <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>  
-              <form method="POST" action={`places/${data.id}?_method=DELETE`}> 
-                <button type="submit" className="btn btn-danger">Delete</button>
-              </form>    
+          </div>
+          <div className="row">
+            <div className="form-group col-sm-4">
+              <label htmlFor="author">Author</label>
+              <input id="author" name="author" className="form-control" />
             </div>
-          </main>
-
-        </Def>
-    )
+            <div className="form-group col-sm-4">
+              <label htmlFor="stars">Star Rating</label>
+              <br />
+              <input
+                type="range"
+                step="0.5"
+                min="1"
+                max="5"
+                id="stars"
+                name="stars"
+              />
+            </div>
+            <div className="form-group col-sm-2">
+              <label htmlFor="rant">Rant?</label>
+              <br />
+              <input type="checkbox" id="rant" name="rant" />
+            </div>
+          </div>
+          <input
+            type="submit"
+            className="btn btn-primary"
+            value="Add Comment"
+          />
+        </form>
+      </main>
+    </Def>
+  );
 }
 
 module.exports = show
