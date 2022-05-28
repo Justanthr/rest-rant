7d1042fbf34d998896f4b5fcf1d5a852ed8e6884
Jennika Wilson <j.r.a.112100@gmail.com>
2022-05-28 14:54:51 -0400

    Added Rating

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -8,6 +8,15 @@ function new_form (data) {
             <h4 className='alert-danger'>{data.message}</h4>
         )
     }
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+        return tot + c.stars
+      }, 0)
+      let averageRating = sumRatings / data.place.comments.length
+      rating = (
+        <h3>
+        {Math.round(averageRating)} stars
+        </h3>
+      )      
     return (
         <Def>
               <main>
